fbf4bba072e40eb4e8e08b9e3423a2e54e345952
Payal <thepayalsoni@gmail.com>
2016-05-30 23:23:52 +0530

    1 Created  login layout skeleton

---
+++ app/src/main/java/com/cvidio/cvidioapp/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.cvidio.cvidioapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by payal on 30/5/16.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
